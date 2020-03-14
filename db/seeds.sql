4ce68e17a654cd2f6eb3291db1c5dd2ea26c73bd
Fung <koufongso@gmail.com>
2020-03-14 01:55:00 -0700

    set up database

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name,dervoured)
+VALUES("Hamburger",false);
+
+INSERT INTO burgers(burger_name,dervoured)
+VALUES("Bacon Cheeseburger ",false);
+
+INSERT INTO burgers(burger_name,dervoured)
+VALUES("Angus Burger",false);
