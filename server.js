cfb82632ee301468002bcc9a1d1bcc1c2a9f176b
Fung <koufongso@gmail.com>
2020-03-14 20:23:15 -0700

    made it be able to handle express-handlebars

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.use(express.static("public"));
+
+var handlebars = require("express-handlebars");
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
