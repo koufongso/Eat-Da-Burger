ebc3b21d0178c09a59c2308df25ac91812c6a03f
Fung <koufongso@gmail.com>
2020-03-14 17:22:40 -0700

    seperated mySQL database connection config

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,21 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var config = {
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'root',
     database: 'burgers_db'
-});
+}
+
+var connection = mysql.createConnection(config);
 
 connection.connect(function(err){
     if(err){
         console.error("error connecting: " + err.stack);
         return 
     }
-    console.log("Connected to database %s as id %d",database, connection.threadId);
+    console.log("Connected to database %s as id %d",config.database, connection.threadId);
 });
 
 module.exports = connection;
