70a8a40b8cf2f1524807f3c83a665dd89165b887
Fung <koufongso@gmail.com>
2020-03-15 01:47:02 -0700

    modified schema.sql for JAWSDB setup

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,7 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
 CREATE TABLE burgers(
 	id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT false,
+    createAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
     PRIMARY KEY(id)
 );
